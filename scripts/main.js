97d8a28942c6c0162202f181c665148fe3958a74
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-01-20 14:47:56 +0700

    Complete step 1 & 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const myLibrary = [];
+
+// the constructor...
+function Book(author, title, pages, readStatus) {
+  if (!new.target) {
+    throw Error("Constructors must be called using the `new` keyword");
+  }
+  this.id = crypto.randomUUID();
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+// take params, create a book then store it in the array
+function addBookToLibrary(author, title, pages, readStatus) {
+  const newBook = new Book(author, title, pages, readStatus);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("J.R.R. Tolkien", "The Lord of The Rings", 1137, "Completed");
+addBookToLibrary("J.K. Rowling", "Harry Potter", 3407, "Completed");
